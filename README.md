dd1e732403b081f6306396936e773134b4717ffa
jlopezlaxague <jlopezlaxague@gmail.com>
2021-02-07 15:03:05 -0300

    routes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Cruce
+
+RUN
+go to "back" folder
+npm intall
+npm run seed
+npm start
+
+ROUTES
--- back/api/routes/index.js
+++ back/api/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const users = require("./users");
+const orders = require("./orders");
 
-router.get("/users", (req, res) => {
-  res.send("prueba users");
-});
+router.use("/users", users);
+router.use("/orders", orders);
 
 //para armar rutas hay que hacer un archivo por cada funcionalidad e importarlo acá
 //luego usar router.use("/funcionalidad", userRoutesEjemplo)
